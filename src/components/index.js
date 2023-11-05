9306e38238e1e18869b2f5c95758e58827dcb161
Saurav Poojary <sauravpoojary@gmail.com>
2023-11-05 18:29:39 +0530

    feat : frontend added

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import Header from "./header/Header";
+import Input from "./Input";
+import Button from "./Button";
+
+export { 
+    Header, 
+    Footer, 
+    Container,
+    Logo, 
+    LogoutBtn, 
+    Input, 
+    Button };
